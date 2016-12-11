b18b36b0daa5818ad022a607e00e9b934fdd5564
Tasha Casagni <casagni708@hotmail.com>
2016-12-11 17:11:51 -0600

    updated routes and put in group photos

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var mysql = require("mysql");
 
 
 // ==============================================================================
@@ -26,6 +27,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/app/public"));
+
+
+// ==============================================================================
+// Mysql CONFIGURATION
+// This sets up the basic connections for our server
+// ==============================================================================
+
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+	connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "fetch_db"
+	});
+
+} 
 
 // ================================================================================
 // ROUTER
