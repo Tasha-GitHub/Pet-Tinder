f7000dbffbf185c41a928d9460792a66f4ab8968
Tasha Casagni <casagni708@hotmail.com>
2016-12-11 18:19:50 -0600

    updated server js file to exclude api routes for now

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ if(process.env.JAWSDB_URL){
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./app/routing/api-routes.js")(app);
+//require("./app/routing/api-routes.js")(app);
 require("./app/routing/html-routes.js")(app);
 
 
