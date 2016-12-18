58c63593aef93e47594807c92bedf07fe5b1ca16
Tasha Casagni <casagni708@hotmail.com>
2016-12-17 22:15:20 -0600

    removed bcypt.js file

--- app/public/javascript/bcrypt.js
+++
@@ -1,9 +0,0 @@
-// // Login API calls
-// $("#loginButton").on("click",function(){
-// 	var credentials = { 
-// 		user_name: $("#emailLogin").val().trim(),
-// 		user_pass: $("#passwordLogin").val().trim()
-// 	}
-// 	console.log(credentials);
-
-// });
