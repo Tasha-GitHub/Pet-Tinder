b85fcc16d3300acc45561f4075d45e1de1d4b1ff
jessephilip <jessephilip@gmail.com>
2017-01-03 17:33:29 -0600

    got login to work with oauth for google

--- app/public/javascript/login.js
+++ app/public/javascript/login.js
@@ -1,156 +1,175 @@
  function onSignIn(googleUser) {
-        // Useful data for your client-side scripts:
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
+     // Useful data for your client-side scripts:
+     var profile = googleUser.getBasicProfile();
+     //console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+     //console.log('Full Name: ' + profile.getName());
+     //console.log('Given Name: ' + profile.getGivenName());
+     //console.log('Family Name: ' + profile.getFamilyName());
+     //console.log("Image URL: " + profile.getImageUrl());
+     //console.log("Email: " + profile.getEmail());
 
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
+     // The ID token you need to pass to your backend:
+     var id_token = googleUser.getAuthResponse().id_token;
 
-        // data from the oatuh signin
-        var info = {
-            id: profile.getId(),
-            fullname: profile.getName(),
-            firstname: profile.getGivenName(),
-            lastname: profile.getFamilyName(),
-            photo: profile.getImageUrl(),
-            email: profile.getEmail()
-        }
+     // data from the oatuh signin
+     var info = {
+         id: profile.getId(),
+         fullname: profile.getName(),
+         firstname: profile.getGivenName(),
+         lastname: profile.getFamilyName(),
+         photo: profile.getImageUrl(),
+         email: profile.getEmail(),
+         password: profile.getId()
+     }
 
-        // id, user_name, user_password, admin, createdAt, createdAt, updatedAt
-        $.ajax({
-            type: "POST",
-            url: "/login",
-            data: info
-        });
-      };
+     // id, user_name, user_password, admin, createdAt, createdAt, updatedAt
 
-    // for modal popup
-    $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
+     $.ajax({
+             url: '/login',
+             type: 'POST',
+             data: info
 
-    // locators
+         })
+         .done(function (googleSuccess) {
+             //console.log("Google SignIn success", googleSuccess);
+             var name = googleSuccess.name;
+             //console.log("googleSuccess", name);
+             var logText = $(".logText");
+             logText.attr("onclick", "signOut()");
+             logText.text("Logged in as: " + name);
+         })
+         .fail(function (failedObject) {
+             console.log("Google SignIn error", failedObject.responseText);
+         });
+ };
 
-    // input fields
-    var emailLoginInput = $("#emailLogin");
-    var passwordLoginInput = $("#passwordLogin");
-    var nameSignUpInput = $("#nameSignUpInput");
-    var phoneSignUpInput = $("#phoneSignUpInput");
-    var emailSignUpInput = $("#emailSignUpInput");
-    var passwordSignUpInput1 = $("#passwordSignUpInput1");
-    var passwordSignUpInput2 = $("#passwordSignUpInput2");
+ // for modal popup
+ $(document).ready(function () {
+     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+     $('.modal').modal();
 
-    // buttons
+     // locators
 
-    var loginButton = $("#loginButton");
-    var signUpButton = $("#signUpButton");
+     // input fields
+     var emailLoginInput = $("#emailLogin");
+     var passwordLoginInput = $("#passwordLogin");
+     var nameSignUpInput = $("#nameSignUpInput");
+     var phoneSignUpInput = $("#phoneSignUpInput");
+     var emailSignUpInput = $("#emailSignUpInput");
+     var passwordSignUpInput1 = $("#passwordSignUpInput1");
+     var passwordSignUpInput2 = $("#passwordSignUpInput2");
 
-    // clicklisteners
+     // buttons
 
-    loginButton.on("click", loginClick);
-    signUpButton.on("click", signUpClick);
+     var loginButton = $("#loginButton");
+     var signUpButton = $("#signUpButton");
 
-    // clicklistener functions
-    function loginClick(e){
-        e.preventDefault();
+     // clicklisteners
 
-        var email = emailLoginInput.val().trim();
-        var password = passwordLoginInput.val().trim();
+     loginButton.on("click", loginClick);
+     signUpButton.on("click", signUpClick);
 
-        var loginObject = {
-            email: email,
-            password: password
-        };
+     // clicklistener functions
+     function loginClick(e) {
+         e.preventDefault();
 
-        emailLoginInput.val("");
-        passwordLoginInput.val("");
-        var currentURL = window.location.origin;
-        $.ajax({
-            type: "POST",
-            url: currentURL + "/login",
-            data: loginObject
-        })
-        .done(function(data){
-            if(data.confirm){
-                // Emptied the localStorage
-                localStorage.clear();
-                // Store all content into localStorage
-                localStorage.setItem("userID", data.result);
-                //reload page
-                location.reload();
-            } else{
-                alertify.error("Invalid Password or Email");
-            }
-        });
+         var email = emailLoginInput.val().trim();
+         var password = passwordLoginInput.val().trim();
 
-    }
+         var loginObject = {
+             email: email,
+             password: password
+         };
 
-        function signUpClick(e){
-            var password1 = passwordSignUpInput1.val().trim();
-            var password2 = passwordSignUpInput2.val().trim();
+         emailLoginInput.val("");
+         passwordLoginInput.val("");
+         var currentURL = window.location.origin;
+         $.ajax({
+                 type: "POST",
+                 url: currentURL + "/login",
+                 data: loginObject
+             })
+             .done(function (data) {
+                 if (data.confirm) {
+                     // Emptied the localStorage
+                     localStorage.clear();
+                     // Store all content into localStorage
+                     localStorage.setItem("userID", data.result);
+                     //reload page
+                     location.reload();
+                 } else {
+                     alertify.error("Invalid Password or Email");
+                 }
+             });
 
-            if (password1 === password2 && password1.length > 3) {
-                e.preventDefault();
-                var password = password1;   
-                var name = nameSignUpInput.val().trim();
-                var email = emailSignUpInput.val().trim();
-                var phone = phoneSignUpInput.val().trim();
-                var password1 = passwordSignUpInput1.val().trim();
-                var password2 = passwordSignUpInput2.val().trim();
+     }
 
-                var signUpObject = {
-                    name: name,
-                    email: email,
-                    phone: phone,
-                    password: password
-                };
-                var currentURL = window.location.origin;
-                console.log(signUpObject);
+     function signUpClick(e) {
+         var password1 = passwordSignUpInput1.val().trim();
+         var password2 = passwordSignUpInput2.val().trim();
 
-                //ajax post to data base to save users email and pass
-                $.ajax({
-                    type: "POST",
-                    url: currentURL + "/create",
-                    data: signUpObject
-                })
-                .done(function(data){
-                    if(data){
-                        // Emptied the localStorage
-                        localStorage.clear();
-                        // Store all content into localStorage
-                        localStorage.setItem("userID", data.result);
-                        //reload page
-                        location.reload();
-                    }
-                });
+         if (password1 === password2 && password1.length > 3) {
+             e.preventDefault();
+             var password = password1;
+             var name = nameSignUpInput.val().trim();
+             var email = emailSignUpInput.val().trim();
+             var phone = phoneSignUpInput.val().trim();
+             var password1 = passwordSignUpInput1.val().trim();
+             var password2 = passwordSignUpInput2.val().trim();
 
-                nameSignUpInput.val("");
-                emailSignUpInput.val("");
-                phoneSignUpInput.val("");
-                passwordSignUpInput1.val("");
-                passwordSignUpInput2.val("");
-            } else if (password1 <= 3) {
-                alertify.error("Your password must be four characters or longer.");
-            } else {
-                alertify.error("Your passwords do not match.");
-                }
-        }
+             var signUpObject = {
+                 name: name,
+                 email: email,
+                 phone: phone,
+                 password: password
+             };
+             var currentURL = window.location.origin;
+             //console.log(signUpObject);
 
-  });
+             //ajax post to data base to save users email and pass
+             $.ajax({
+                     type: "POST",
+                     url: currentURL + "/create",
+                     data: signUpObject
+                 })
+                 .done(function (data) {
+                     if (data) {
+                         // Emptied the localStorage
+                         localStorage.clear();
+                         // Store all content into localStorage
+                         localStorage.setItem("userID", data.result);
+                         //reload page
+                         location.reload();
+                     }
+                 });
 
-// javascript for overlay
+             nameSignUpInput.val("");
+             emailSignUpInput.val("");
+             phoneSignUpInput.val("");
+             passwordSignUpInput1.val("");
+             passwordSignUpInput2.val("");
+         } else if (password1 <= 3) {
+             alertify.error("Your password must be four characters or longer.");
+         } else {
+             alertify.error("Your passwords do not match.");
+         }
+     }
 
-/* Open when someone clicks on the span element */
-        function openNav() {
-            document.getElementById("myNav").style.height = "100%";
-        }
+ });
 
-        /* Close when someone clicks on the "x" symbol inside the overlay */
-        function closeNav() {
-            document.getElementById("myNav").style.height = "0%";
-        }
+ // javascript for overlay
+
+ /* Open when someone clicks on the span element */
+ function openNav() {
+     document.getElementById("myNav").style.height = "100%";
+ }
+
+ /* Close when someone clicks on the "x" symbol inside the overlay */
+ function closeNav() {
+     document.getElementById("myNav").style.height = "0%";
+ }
+
+ // this function will signout the current user
+ function signOut() {
+     alertify.error("This function will signout the current user.");
+ }
