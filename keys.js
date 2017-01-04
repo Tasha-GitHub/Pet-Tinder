97926f2324c2d73863860a1c1f58b5a278472836
Tasha Casagni <casagni708@hotmail.com>
2017-01-04 09:34:24 -0600

    fixed keys.js

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 // google credentials
 var google = {
-    clientID: "process.env.GOOGLE_CLIENT_ID",
-    clientSecret: "process.env.GOOGLE_CLIENT_SECRET"
+    clientID: "process.env.GOOGLE_CLIENT_ID" || "325217804379-ho689du9mueiu3q688lpspcsqurf03u7.apps.googleusercontent.com",
+    clientSecret: "process.env.GOOGLE_CLIENT_SECRET" || "WwyaglsbE2MNSrxLm_uVz1hb"
 };
 
 var facebook = {
-    appID: "process.env.FB_APP_ID",
-    appSecret: "process.env.FB_APP_SECRET"
+    appID: "process.env.FB_APP_ID" || "1537854722909149",
+    appSecret: "process.env.FB_APP_SECRET" || "2b5f30569ac36fa4c6445466cff52b13"
 };
