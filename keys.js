abaed1ace4c7669d04b6c74af76d5e357fef6047
Tasha Casagni <casagni708@hotmail.com>
2017-01-04 13:41:02 -0600

    keys update

--- keys.js
+++ keys.js
@@ -3,8 +3,3 @@ var google = {
     clientID: "325217804379-ho689du9mueiu3q688lpspcsqurf03u7.apps.googleusercontent.com",
     clientSecret: "WwyaglsbE2MNSrxLm_uVz1hb"
 };
-
-var facebook = {
-    appID: "1537854722909149",
-    appSecret: "2b5f30569ac36fa4c6445466cff52b13"
-};
